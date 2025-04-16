d9f657b98a5fdd4da43b2181ab9b7567a6b44d78
potatopudding-SSKS <kssaisankalp.davey@research.iiit.ac.in>
2025-04-16 15:49:27 +0530

    Disabled CORS

--- backend/main.py
+++ backend/main.py
@@ -1,13 +1,23 @@
 from fastapi import FastAPI
 from routes.items import router as items_router
+from fastapi.middleware.cors import CORSMiddleware
 from routes.analytics import router as analytics_router
 from routes.quiz import router as quiz_router
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allow all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all methods
+    allow_headers=["*"],  # Allow all headers
+)
+
 app.include_router(items_router, prefix="/items")
 app.include_router(analytics_router, prefix="/analytics")
 app.include_router(quiz_router, prefix="/quiz")
+
 
 # why the hell did I write this function?
 @app.get("/home")
