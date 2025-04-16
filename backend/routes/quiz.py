caacbeb64492c38e3928392c6296f3c797f52cf3
gakshat365 <akshat.g@research.iiit.ac.in>
2025-04-16 15:03:26 +0530

    ak4 made questions random in quiz.py

--- backend/routes/quiz.py
+++ backend/routes/quiz.py
@@ -41,7 +41,7 @@ game_state = {"high_score": 0}
 # god would hate me for not dockerizing this repo
 @router.get("/question")
 async def get_question():
-    question = questions[1]
+    question = random.choice(questions)
     return {
         "id": question["id"],
         "text": question["text"],
