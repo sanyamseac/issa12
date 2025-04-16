4ec4da18f5986588c7518b302e59c06337c73eb5
potatopudding-SSKS <kssaisankalp.davey@research.iiit.ac.in>
2025-04-16 15:54:28 +0530

    Changed 8k1 to 8k

--- frontend/scripts/profile.js
+++ frontend/scripts/profile.js
@@ -1,4 +1,4 @@
-const baseURL = "http://localhost:8001";
+const baseURL = "http://localhost:8000";
 async function loadUsers() {
   const res = await fetch(`${baseURL}/users`);
   const users = await res.json();
