08981b746e411ee3220bacd485c9f794e9699a14
potatopudding-SSKS <kssaisankalp.davey@research.iiit.ac.in>
2025-04-16 15:20:17 +0530

    edit models.py

--- backend/models.py
+++ backend/models.py
@@ -1,11 +1,13 @@
 from pydantic import BaseModel
 
+
 class Item(BaseModel):
-    name: int
+    name: str
     description: str
+
 
 class User(BaseModel):
     username: str
     bio: str
-    
+
     # You can raise your hands and give the answer to the chocolate question
