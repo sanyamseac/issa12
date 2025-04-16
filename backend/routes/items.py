04a69d49ddee8c8f1ddc885a8e4c16d3de86587d
sanyamseac <contact@sanyamseac.rf.gd>
2025-04-16 15:18:28 +0530

    fix router

--- backend/routes/items.py
+++ backend/routes/items.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, HTTPException
 from models import Item
 from bson import ObjectId
 
-router = {}
+router = APIRouter(tags=["items"])
 
 async def get_items_collection():
     from db import init_db
@@ -23,10 +23,6 @@ async def create_item(item: Item):
     result = await collection.insert_one(item.dict())
     return {"id": str(result.inserted_id)}
 
-@router.post("/")
-async def create_item(item: Item):
-    return {"id": "Item Inserted"}
-# I want a chocolate
 @router.delete("/{item_id}/{item_details}")
 async def delete_item(item_id: str, item_details:str):
     collection = await get_items_collection()
